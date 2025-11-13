f6ba666c35dc24cccfab939fee43333d619aae55
lucasojeda <wichisan@hotmail.com>
2025-11-13 17:01:09 -0300

    tarea

--- src/Screens/LoginScreen/LoginScreen.jsx
+++ src/Screens/LoginScreen/LoginScreen.jsx
@@ -1,20 +1,23 @@
-import React, { useState } from 'react'
-import useForm from '../../hooks/useForm'
-import { register } from '../../services/authService'
-import useFetch from '../../hooks/useFetch'
+import React, { useEffect } from 'react'
+import useFetch from '../../hooks/useFetch.jsx'
+import useForm from '../../hooks/useForm.jsx'
+import {login} from '../../services/authService.js'
+import { useNavigate } from 'react-router'
+import LOCALSTORAGE_KEYS from '../../constants/localstorage.js'
 
 const FORM_FIELDS = {
-    NAME: 'name',
     EMAIL: 'email',
     PASSWORD: 'password'
 }
+
 const initial_form_state = {
-    [FORM_FIELDS.NAME]: '',
     [FORM_FIELDS.EMAIL]: '',
     [FORM_FIELDS.PASSWORD]: ''
 }
 
-const RegisterScreen = () => {
+export const LoginScreen = () => {
+
+    const navigate = useNavigate()
 
     const {
         sendRequest,
@@ -22,48 +25,44 @@ const RegisterScreen = () => {
         response,
         error
     } = useFetch()
+    
+    const onLogin = (form_state) => {
+        sendRequest(() => login(
+            form_state[FORM_FIELDS.EMAIL],
+            form_state[FORM_FIELDS.PASSWORD]
+        ))
+    }
 
-    const onRegister = (form_state) => {
-        sendRequest( () => {
-            register(
-                form_state[FORM_FIELDS.NAME], 
-                form_state[FORM_FIELDS.EMAIL], 
-                form_state[FORM_FIELDS.PASSWORD]
-            )
-        })
-    }   
+    useEffect(
+      () =>{
+        console.log(response)
+        if(response && response.ok){
+          //Guardamos el token emitido por el backend, para despues usarlo como credencial
+          localStorage.setItem(LOCALSTORAGE_KEYS.AUTH_TOKEN, response.data.authorization_token)
+          navigate('/home')
+        }
+        
+      },
+      [response]
+    )
 
     const {
-        form_state: register_form_state,
+        form_state: login_form_state,
         handleSubmit,
         handleInputChange
     } = useForm(
         {
             initial_form_state,
-            onSubmit: onRegister
+            onSubmit: onLogin
         }
     )
 
-
-    console.log(loading)
     return (
         <div>
-            <h1>Registrate</h1>
+            <h1>Iniciar Sesión</h1>
             <form onSubmit={handleSubmit}>
-
                 <div>
-                    <label htmlFor={FORM_FIELDS.NAME} >Nombre:</label>
-                    <input
-                        name={FORM_FIELDS.NAME}
-                        id={FORM_FIELDS.NAME}
-                        type='text'
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-
-
-                    <label htmlFor={FORM_FIELDS.EMAIL} >Email:</label>
+                    <label htmlFor={FORM_FIELDS.EMAIL}>Email:</label>
                     <input
                         name={FORM_FIELDS.EMAIL}
                         id={FORM_FIELDS.EMAIL}
@@ -72,7 +71,7 @@ const RegisterScreen = () => {
                     />
                 </div>
                 <div>
-                    <label htmlFor={FORM_FIELDS.PASSWORD} >Contraseña:</label>
+                    <label htmlFor={FORM_FIELDS.PASSWORD}>Contraseña:</label>
                     <input
                         name={FORM_FIELDS.PASSWORD}
                         id={FORM_FIELDS.PASSWORD}
@@ -82,20 +81,20 @@ const RegisterScreen = () => {
                 </div>
                 {
                     !response
-                    ? <button type='submit' disabled={loading}>Registrarse</button>
-                    : <>
-                        <button type='submit' disabled={true}>Registrado</button>
-                        <span style={{color: 'green'}}>{response.message}</span>
-                    </>
+                        ?
+                        <button type='submit' disabled={loading}>
+                            {loading ? 'Iniciando sesión...' : 'Iniciar Sesión'}
+                        </button>
+                        :
+                        <>
+                            <button type='submit' disabled={true}>Sesión Iniciada</button>
+                            <span style={{ color: 'green' }}>{response.message}</span>
+                        </>
                 }
                 {
-                    error && <span style={{color: 'red'}}>{error.message}</span>
+                    error && <span style={{ color: 'red' }}>{error.message}</span>
                 }
-               
-                
             </form>
         </div>
     )
 }
-
-export default RegisterScreen
--- src/Screens/RegisterScreen/RegisterScreen.jsx
+++ src/Screens/RegisterScreen/RegisterScreen.jsx
@@ -1,23 +1,20 @@
-import React, { useEffect } from 'react'
-import useFetch from '../../hooks/useFetch.jsx'
-import useForm from '../../hooks/useForm.jsx'
-import {login} from '../../services/authService.js'
-import { useNavigate } from 'react-router'
-import LOCALSTORAGE_KEYS from '../../constants/localstorage.js'
+import React, { useState } from 'react'
+import useForm from '../../hooks/useForm'
+import { register } from '../../services/authService'
+import useFetch from '../../hooks/useFetch'
 
 const FORM_FIELDS = {
+    NAME: 'name',
     EMAIL: 'email',
     PASSWORD: 'password'
 }
-
 const initial_form_state = {
+    [FORM_FIELDS.NAME]: '',
     [FORM_FIELDS.EMAIL]: '',
     [FORM_FIELDS.PASSWORD]: ''
 }
 
-export const LoginScreen = () => {
-
-    const navigate = useNavigate()
+const RegisterScreen = () => {
 
     const {
         sendRequest,
@@ -25,44 +22,48 @@ export const LoginScreen = () => {
         response,
         error
     } = useFetch()
-    
-    const onLogin = (form_state) => {
-        sendRequest(() => login(
-            form_state[FORM_FIELDS.EMAIL],
-            form_state[FORM_FIELDS.PASSWORD]
-        ))
-    }
 
-    useEffect(
-      () =>{
-        console.log(response)
-        if(response && response.ok){
-          //Guardamos el token emitido por el backend, para despues usarlo como credencial
-          localStorage.setItem(LOCALSTORAGE_KEYS.AUTH_TOKEN, response.data.authorization_token)
-          navigate('/home')
-        }
-        
-      },
-      [response]
-    )
+    const onRegister = (form_state) => {
+        sendRequest( () => {
+            register(
+                form_state[FORM_FIELDS.NAME], 
+                form_state[FORM_FIELDS.EMAIL], 
+                form_state[FORM_FIELDS.PASSWORD]
+            )
+        })
+    }   
 
     const {
-        form_state: login_form_state,
+        form_state: register_form_state,
         handleSubmit,
         handleInputChange
     } = useForm(
         {
             initial_form_state,
-            onSubmit: onLogin
+            onSubmit: onRegister
         }
     )
 
+
+    console.log(loading)
     return (
         <div>
-            <h1>Iniciar Sesión</h1>
+            <h1>Registrate</h1>
             <form onSubmit={handleSubmit}>
+
                 <div>
-                    <label htmlFor={FORM_FIELDS.EMAIL}>Email:</label>
+                    <label htmlFor={FORM_FIELDS.NAME} >Nombre:</label>
+                    <input
+                        name={FORM_FIELDS.NAME}
+                        id={FORM_FIELDS.NAME}
+                        type='text'
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+
+
+                    <label htmlFor={FORM_FIELDS.EMAIL} >Email:</label>
                     <input
                         name={FORM_FIELDS.EMAIL}
                         id={FORM_FIELDS.EMAIL}
@@ -71,7 +72,7 @@ export const LoginScreen = () => {
                     />
                 </div>
                 <div>
-                    <label htmlFor={FORM_FIELDS.PASSWORD}>Contraseña:</label>
+                    <label htmlFor={FORM_FIELDS.PASSWORD} >Contraseña:</label>
                     <input
                         name={FORM_FIELDS.PASSWORD}
                         id={FORM_FIELDS.PASSWORD}
@@ -81,20 +82,20 @@ export const LoginScreen = () => {
                 </div>
                 {
                     !response
-                        ?
-                        <button type='submit' disabled={loading}>
-                            {loading ? 'Iniciando sesión...' : 'Iniciar Sesión'}
-                        </button>
-                        :
-                        <>
-                            <button type='submit' disabled={true}>Sesión Iniciada</button>
-                            <span style={{ color: 'green' }}>{response.message}</span>
-                        </>
+                    ? <button type='submit' disabled={loading}>Registrarse</button>
+                    : <>
+                        <button type='submit' disabled={true}>Registrado</button>
+                        <span style={{color: 'green'}}>{response.message}</span>
+                    </>
                 }
                 {
-                    error && <span style={{ color: 'red' }}>{error.message}</span>
+                    error && <span style={{color: 'red'}}>{error.message}</span>
                 }
+               
+                
             </form>
         </div>
     )
 }
+
+export default RegisterScreen
