469330e9c56009cb055d191a899fc4a017a2551f
lucasojeda <wichisan@hotmail.com>
2025-11-13 17:19:40 -0300

    tarea4

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
 import React from 'react'
-import { Route, Routes, Navigate  } from 'react-router'
-
-
+import { Route, Routes, Navigate } from 'react-router'
 import AuthMiddleware from './Middlewares/AuthMiddleware'
 import ResetPasswordScreen from './Screens/ResetPasswordScreen/ResetPasswordScreen.jsx'
 import RecoveryScreen from './Screens/RecoveryScreen/RecoveryScreen'
 import RegisterScreen from './Screens/RegisterScreen/RegisterScreen.jsx'
 import LoginScreen from './Screens/LoginScreen/LoginScreen.jsx'
+
 function App() {
-
   return (
-
     <Routes>
-
       <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path='/register' element={<RegisterScreen/>} />
-      <Route path='/login' element={<LoginScreen/>} />
+      <Route path="/register" element={<RegisterScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
       <Route path="/recovery" element={<RecoveryScreen />} />
       <Route path="/reset-password/:recovery_token" element={<ResetPasswordScreen />} />
-      <Route element={AuthMiddleware}/>
-       <Route path='/home' element={<h1>home</h1>}/>
-     
-    </Routes>
 
+      {/* 🔒 Rutas protegidas */}
+      <Route element={<AuthMiddleware />}>
+        <Route path="/home" element={<h1>home</h1>} />
+      </Route>
+    </Routes>
   )
 }
 
