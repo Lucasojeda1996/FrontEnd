90c9ebdc15fbe75fbba3ee9be0edcd86043c3358
lucasojeda <wichisan@hotmail.com>
2025-11-14 14:47:07 -0300

    tarea223

--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -4,32 +4,33 @@ import { getWorkspaceList } from '../../services/workspaceService'
 import { Link } from 'react-router'
 
 const HomeScreen = () => {
-    const {loading, response, error, sendRequest} = useFetch()
+    const { loading, response, error, sendRequest } = useFetch()
 
-    useEffect(
-        () => {
-            sendRequest(
-                getWorkspaceList
-            )
-        },
-        []
-    )
+    useEffect(() => {
+        sendRequest(getWorkspaceList)
+    }, [])
 
     console.log(response, loading, error)
-  return (
-  <div>
-  <h1>Espacios de trabajo</h1>
 
-  {response?.data?.workspaces?.map((mw) => (
-    <div key={mw._id}>
-      <h2>{mw.workspace?.name}</h2>
-      <a href={`/workspace/${mw.workspace?._id}`}>Entrar</a>
-    </div>
-  ))}
+    return (
+        <div>
+            <h1>Espacios de trabajo</h1>
 
-  <Link to='/workspace/new'>Crear nuevo espacio de trabajo</Link>
-</div>
-  )
+            {
+                !loading &&
+                response?.data?.workspaces?.map((mw) => (
+                    <div key={mw._id}>
+                        <h2>{mw.workspace?.name}</h2>
+                        <a href={`/workspace/${mw.workspace?._id}`}>Entrar</a>
+                    </div>
+                ))
+            }
+
+            <Link to={'/workspace/new'}>
+                Crear nuevo espacio de trabajo
+            </Link>
+        </div>
+    )
 }
 
 export default HomeScreen
